d05b158f8fcae71a55da0002d1b82b8398f5ef21
Bjørn Sødal <bjornso@uia.no>
2025-08-18 14:13:26 +0200

    Task IV

--- assignment_1/main.py
+++ assignment_1/main.py
@@ -9,5 +9,15 @@ print("The image has", img.shape[2], "channels")
 print("The size of the total image is:", img.size, "pixels, for single channel:", img.size / 3)
 print("The image is of type:", img.dtype)
 
+cam = cv.VideoCapture(0)
 
+fps = int(cam.get(cv.CAP_PROP_FPS))
+width = int(cam.get(cv.CAP_PROP_FRAME_WIDTH))
+height = int(cam.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+file_name = "camera_outputs.txt"
+
+write_fps = "FPS: ", fps
+write_width = "Width: ", width
+write_height = "Height: ", height
 
